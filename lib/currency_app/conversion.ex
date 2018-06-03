6d8e3b11a14d3757a5ec33fb70aedfbb12a4c7b4
Wende Lu <wendelu1991@gmail.com>
2018-06-03 15:24:10 +0800

    finish

---
+++ lib/currency_app/conversion.ex
@@ -0,0 +1,24 @@
+defmodule CurrencyApp.Conversion do
+  def from_euro_to_dollar(amount) do
+    url = "cs-currency-rates.herokuapp.com/currency-rates"
+    case HTTPoison.get(url) do
+      {:ok, response} -> parse(response) |> convert(amount)
+      {:error, _} -> "Error fetching rates"
+    end
+  end
+
+  defp parse(%{status_code: 200, body: json_response}) do
+    Poison.Parser.parse(json_response)
+  end
+
+  defp convert({:ok, rates}, amount) do
+    rate = find_euro(rates)
+    amount * rate
+  end
+
+  defp find_euro([%{"currency" => "euro", "rate" => rate} | _]), do: rate
+  defp find_euro([_ | tail]), do: find_euro(tail)
+  defp find_euro([]), do: raise "No rate found for Euro"
+end
+
+# mix run -e "CurrencyApp.Conversion.from_euro_to_dollar(15) |> IO.puts"
